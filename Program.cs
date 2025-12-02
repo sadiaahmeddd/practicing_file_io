b8a7a1691695af86edb4309ddcd6224cf22308b9
sa7964 <sa7964@rit.edu>
2025-12-01 20:19:52 -0500

    setup for practicing_io

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//seup
+﻿using System;
+using System.IO;
+
+namespace practicing_file_io
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //  Creating a file
+            FileStream stream = File.Create("my_badass_file.txt");
+
+            // Close the stream
+            stream.Close();
+
+            //  Appending text to the file
+            File.AppendAllText("my_badass_file.txt",
+                "I am the programmer, and I say let there be text in this badass file!");
+
+            
+            Console.WriteLine("File created and text written successfully!");
+        }
+    }
+}
